7e957ac3ffb1a4c932ee6972dcd2cf58217d5bb6
Ricky Mach <machricky2002@gmail.com>
2024-11-09 14:21:42 -0500

    initial commit of interpreter function

--- lang.py
+++ lang.py
@@ -1,1 +1,13 @@
-# test
+# Aaron, Harpreet, Ricky - 
+
+def interpret_code(code):
+    if code.startswith("P "):
+        # extract text
+        text = code[2:].strip().strip('<').strip('>')
+        print(text)
+    else:
+        print("Error: Unknown command")
+
+someCode = "P <sup dog>"
+
+interpret_code(someCode)
