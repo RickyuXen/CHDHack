5454befc5d47b9b18e73944deebf03370510cc4d
Ricky Mach <machricky2002@gmail.com>
2024-11-09 15:13:56 -0500

    changes

--- lang.py
+++ lang.py
@@ -2,25 +2,27 @@
 
 # Wrap functionality around class
 class Lang:
-    def __init__(self, start, left_delim, right_delim):
-        self.start = start
+    def __init__(self, left_delim, right_delim):
         self.left_delim = left_delim
         self.right_delim = right_delim
 
     def interpret_code(self, code):
-        if code.startswith(self.start):
+        if code.startswith('P '):
             # extract text
             try:
                 text = code[2:].strip().strip(self.left_delim).strip(self.right_delim)
+                return text
             except:
                 print("Error: Unknown command")
-        return text
-    
+        else:
+                print("Error: Unknown command")
 
+print(""" Syntax:
+      P <[input]> -> prints out [input]
+""")
+someCode = input()
 
-someCode = "P <sup dog>"
-
-test = Lang('P ', '<', '>')
+test = Lang('<', '>')
 
 res = test.interpret_code(someCode)
 print(res)
