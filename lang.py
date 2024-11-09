d72861064711e0ee76f07990e2494b079f845e50
Ricky Mach <machricky2002@gmail.com>
2024-11-09 18:22:18 -0500

    changes

---
+++ lang.py
@@ -0,0 +1,239 @@
+# Aaron, Harpreet, Ricky - 
+ascii_alphabet = {
+    'A': [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1]
+    ],
+    'B': [
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0]
+    ],
+    'C': [
+        [0, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1]
+    ],
+    'D': [
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0]
+    ],
+    'E': [
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1]
+    ],
+    'F': [
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0]
+    ],
+    'G': [
+        [0, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0],
+        [1, 0, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 1]
+    ],
+    'H': [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1]
+    ],
+    'I': [
+        [1, 1, 1, 1, 1],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [1, 1, 1, 1, 1]
+    ],
+    'J': [
+        [0, 0, 1, 1, 1],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0]
+    ],
+    'K': [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 1, 0],
+        [1, 1, 1, 0, 0],
+        [1, 0, 0, 1, 0],
+        [1, 0, 0, 0, 1]
+    ],
+    'L': [
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1]
+    ],
+    'M': [
+        [1, 0, 0, 0, 1],
+        [1, 1, 0, 1, 1],
+        [1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1]
+    ],
+    'N': [
+        [1, 0, 0, 0, 1],
+        [1, 1, 0, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1]
+    ],
+    'O': [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0]
+    ],
+    'P': [
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0]
+    ],
+    'Q': [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1],
+        [0, 1, 1, 1, 1]
+    ],
+    'R': [
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0],
+        [1, 0, 1, 0, 0],
+        [1, 0, 0, 1, 0]
+    ],
+    'S': [
+        [0, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0]
+    ],
+    'T': [
+        [1, 1, 1, 1, 1],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0]
+    ],
+    'U': [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0]
+    ],
+    'V': [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0]
+    ],
+    'W': [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 1, 0, 1, 1],
+        [1, 0, 0, 0, 1]
+    ],
+    'X': [
+        [1, 0, 0, 0, 1],
+        [0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 1, 0, 1, 0],
+        [1, 0, 0, 0, 1]
+    ],
+    'Y': [
+        [1, 0, 0, 0, 1],
+        [0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0]
+    ],
+    'Z': [
+        [1, 1, 1, 1, 1],
+        [0, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0],
+        [1, 1, 1, 1, 1]
+    ]
+}
+
+
+# Wrap functionality around class
+class Lang:
+    def __init__(self):
+        self.command = ""
+
+    def print_letter(self, letter_matrix):
+        for row in letter_matrix:
+            line = ''.join(['#' if col == 1 else ' ' for col in row])
+            print(line)
+    
+    def parse_code(self, code):
+        self.command = code.split("|")
+        print(self.command)
+
+    def interpret_code(self, code):
+        self.parse_code(code)
+        com = self.command[0]
+        match com:
+            case "P":
+                print(self.command[1])
+            case "Pr":
+                print(self.command[1][::-1])
+            case "Pri":
+                print(self.command[1][::-1][::-1])
+            case "Pa":
+                eachLetter = list(self.command[1].replace(" ", "").upper())
+                
+                # Initialize a list to hold each row of the final output
+                output_rows = [""] * 5  # Assuming each letter is 5 rows tall
+
+                for letter in eachLetter:
+                    if letter in ascii_alphabet:
+                        letter_rows = self.print_letter(ascii_alphabet[letter])
+                        # Append each row of the letter to the corresponding row in output
+                        for i in range(5):  # Assuming each letter has 5 rows
+                            output_rows[i] += letter_rows[i] + " "  # Add a space between letters
+                    else:
+                        return "Error: Letter not found"
+
+                # Join all rows with newlines for the final output
+                return "\n".join(output_rows)
+
+print(""" Syntax:
+      P|[input] -> prints out [input]
+      Pr|[input] -> prints out [input] in reverse
+      Pri|[input] -> prints out [input] in reverse
+      Pa|[input] -> prints out [input] in ASCII
+""")
+someCode = input()
+
+test = Lang()
+res = test.interpret_code(someCode)
